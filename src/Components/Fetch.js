0136914f3c1af74ba105ecb096527da7db01d0c0
ricRobo4 <artistr3@proton.me>
2023-03-23 15:40:59 -0700

    Added Comments to explain parts of the code Added description variable to display description of weather

--- src/Components/Fetch.js
+++ src/Components/Fetch.js
@@ -4,26 +4,63 @@ import { prod, dev } from './Environment.js'
 
 // const [latitude, setLatitude] = useState('');
 
+// This string stores the part of the API URL that comes right before the API key, which is stored in a different file in order to make sure the API key doesn't end up in the Github repository
 let apiKey = '&appid=';
 
-let coordinates;
+
 let lat = 37.9577;
 let lon = -121.2908;
 
+
+// This code below is a ternary operator that will decide if the api key is hidden or not
 prod.isLive ? apiKey += prod.apiKey : apiKey += dev.apiKey;
 
+// function GetRandomCoord() {
+//     let randomNum = Math.floor(Math.random() * 5);
+
+//     switch(randomNum)
+//     {
+//         case 0:
+//             lat = 37.9577;
+//             lon = -121.2908;
+//             break;
+//         case 1:
+//             lat = 34.0633;
+//             lon = -117.6509;
+//             break;
+//         case 2:
+//             lat = 51.5085;
+//             lon = -0.1257;
+//             break;
+//         case 3:
+//             lat = 35.6895;
+//             lon = 139.6917;
+//             break;
+//     }
+// }
+
+// const [ coordinates, setCoordinates ] = useState('');
+
+// This fetch is meant to take the name of a city typed by the user and use it to get the coordinates, which will then be plugged into the URL of the other fetch function to allow the search function to work. I haven't been able to get it to work so far, though.
 async function GetCoord() {
-    const promise = await fetch(`api.openweathermap.org/data/2.5/weather?q=manteca${apiKey}`);
+    const promise = await fetch(`api.openweathermap.org/data/2.5/weather?q=manteca${apiKey}&units=imperial`);
     const data = await promise.json();
-    coordinates = data;
+    // setCoordinates(data.coord);
+    // coordinates = data;
     // console.log(searchTerm.toLowerCase());
-    return coordinates;
+    // return coordinates;
 }
 
-async function GetWeather() {
+// const promise2 = await fetch (`api.openweathermap.org/data/2.5/weather?q=manteca${apiKey}&units=imperial`);
+// const coordData = await promise2.json();
 
+// console.log(coordData);
+
+// This is the function that will fetch the data from the weather API. The API key is a value that will be plugged in through string interpolation, which allows it to be hidden from GitHub. The coordinates, which are latitude and longitude, are also values that are plugged in through string interpolation, which should hopefully allow the search function to work, if I can get the other fetch working.
+async function GetWeather() {
     const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}${apiKey}&units=imperial`);
     const data = await promise.json();
+    // The comment below was a console log to test if the data was coming through. Since I know it is, the console log wasn't needed anymore.
     // console.log(data);
     return data;
 }
--- src/Components/MainSection/MainSection.js
+++ src/Components/MainSection/MainSection.js
@@ -1,15 +1,22 @@
 import { Container, Row, Col, Form, Button, Navbar, Modal } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import { GetWeather } from '../Fetch.js';
+// The import statements above import several things that are needed to create this web app. The GetWeather function fetches the data from the API, which is one of the most important parts of the project. There are also the React Bootstrap elements, which are needed to build up the scaffolding for the page. Lastly, there are the useState and useEffect hooks, which are necessary to have the data show up correctly on the page.
 
+// This function contains most of the logic for this weather app, as well as a return for the JSX elements that will show up in the browser when the user views the app
 function MainSection() {
     let weatherData = '';
     const [weather, setWeather] = useState('');
     const [temp, setTemp] = useState('');
     const [iconCode, setIconCode] = useState('');
     const [location, setLocation] = useState('');
+    const [description, setDescription] = useState('');
+
+
     const [searchBar, setSearchBar] = useState('');
     const [searchTerm, setSearchTerm] = useState(searchBar);
+
+    let favsHasItems = false;
 
     const handleSearch = (e) => {
         setSearchBar(e.target.value);
@@ -21,11 +28,13 @@ function MainSection() {
         // GetCoord(searchTerm);
     }
 
+    // The code below get the current date, specifically the day of the week, allowing it to be displayed in the app.
     const d = new Date();
     let day = d.getDay();
     let today = '';
     // console.log(day);
 
+    // Since the day of the week is returned as a number, the switch statement below will examine which number it is, and then assign the name of the current day to the "today" string variable, which will then be displayed on the page in the browser, along with the weather data.
     switch(day) {
         case 0:
             today = 'Sunday';
@@ -63,6 +72,7 @@ function MainSection() {
     // console.log(weather);
     // weatherData = weather;
 
+    // This useEffect hook takes each useState for each variable that is needed, and assigns it a value that has been fetched from the OpenWeatherMap API. Dot Notation is then used to make sure the correct data from the API is displayed in the browser.
     useEffect(() => {
         (async () => {
             weatherData = await GetWeather();
@@ -70,12 +80,13 @@ function MainSection() {
             setTemp(weatherData.main.temp);
             setIconCode(weatherData.weather[0].icon);
             setLocation(weatherData.name);
+            setDescription(weatherData.weather[0].description);
             // setIcon('https://openweathermap.org/img/wn/' + iconCode + '@2x.png');
             // console.log(weatherData);
         })();
     }, []);
 
-
+    // The useState below decides wether or not the modal displaying the favorites list will be visible on screen
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -96,28 +107,22 @@ function MainSection() {
                 </Modal.Header>
                 <Modal.Body>
                     <Container>
+                    {
+                        // The ternary operator below decides if the Favorites modal will display the favorites item, if there are any, or a message stating "You do not currently have any favorites" if there are none.
+                        favsHasItems ? 
+                        <Row>
+                            <Col className='favoritesItem'>
+                                
+                            </Col>
+                        </Row>
+                        :
                         <Row>
                             <Col className="d-flex justify-content-center">
-                                <p>(These are placeholders)</p>
+                                <p>You do not currently have any favorites.</p>
                             </Col>
                         </Row>
-                        <Row className='favoritesList'>
-                            <Col className='favoritesItem'>
-                                <h5>Stockton, CA</h5>
-                                <p>52</p>
-                                <p>Raining</p>
-                            </Col>
-                            <Col className='favoritesItem'>
-                                <h5>Manteca, CA</h5>
-                                <p>51</p>
-                                <p>Raining</p>
-                            </Col>
-                            <Col className='favoritesItem'>
-                                <h5>City, ST</h5>
-                                <p>00</p>
-                                <p>Weather</p>
-                            </Col>
-                        </Row>
+                    }
+                        
                     </Container>
                 </Modal.Body>
                 <Modal.Footer>
@@ -133,7 +138,7 @@ function MainSection() {
                     <Form.Group className="mb-3">
                         <Form.Control type="text" placeholder="Search for a Location" onChange={handleSearch}/>
                     </Form.Group>
-                    <Button variant="primary" onClick={handleSubmit}>
+                    <Button variant="secondary" onClick={handleSubmit}>
                         Search
                     </Button>
                 </Form>
@@ -142,13 +147,17 @@ function MainSection() {
             <Container>
                 <Row className='topWeather'>
                     <Col className='weatherDataLeft'>
+                        {/* The HTML tags below have the variables for the weather data inside of them, which are between curly brackets so that they can be placed in the middle of JSX code, being javascript variables */}
                         <h2>{today}</h2>
-                        <h1>{Math.floor(temp)}°</h1>
-                        <h2>{weather}</h2>
+                        {/* The temperature is displayed as a decimal, so Math.floor is used to round it down to a whole number */}
+                        <h1>{Math.floor(temp)} °F</h1>
                         <h2>{location}</h2>
+                        <h2>{weather}</h2>
+                        <p>{description}</p>
+                        <Button variant="warning">Add to Favorites</Button>{' '}
                     </Col>
                     <Col className='weatherDataRight'>
-                        <img src={'https://openweathermap.org/img/wn/' + iconCode + '@4x.png'} />
+                        <img alt='An icon depicting the current weather' src={'https://openweathermap.org/img/wn/' + iconCode + '@4x.png'} />
                     </Col>
                 </Row>
             </Container>
